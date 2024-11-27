783a8db3d98d33b3bcebeb66127f8e874834a98c
Arkadiusz Buchholz <buchholzarkadiusz@gmail.com>
2024-11-27 15:04:44 +0100

    Remove creds from repo

--- scripts/get-app-version.mjs
+++ scripts/get-app-version.mjs
@@ -2,10 +2,10 @@ import { readdirSync } from 'node:fs';
 import path from 'node:path';
 
 const getAppVersion = () => {
-  const dirname = path.dirname(new URL(import.meta.url).pathname, '../apps');
+  const dirname = path.dirname(new URL(import.meta.url).pathname);
   const filePath = path.resolve(dirname, '../apps');
   const files = readdirSync(filePath);
   return files.filter((file) => file.endsWith('.zip'))[0].slice(10, 17);
 };
 
-getAppVersion();
+console.log(getAppVersion());
