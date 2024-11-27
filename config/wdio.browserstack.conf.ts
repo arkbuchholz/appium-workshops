8e3f9bbff80a32cf663ddb670950d5f4463f2fa8
Arkadiusz Buchholz <buchholzarkadiusz@gmail.com>
2024-11-27 14:49:01 +0100

    Remove creds from repo

--- config/wdio.browserstack.conf.ts
+++ config/wdio.browserstack.conf.ts
@@ -1,26 +1,32 @@
-import { config as baseConfig } from './wdio.shared.conf'
-// import type { Options } from '@wdio/types'
+import { config as baseConfig } from './wdio.shared.conf';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 export const config = {
-    ...baseConfig,
-    user: process.env.BROWSERSTACK_USERNAME || 'arkadiuszbuchhol_kFoqdw',
-    key: process.env.BROWSERSTACK_ACCESS_KEY || 'PAcyyvVWFDo9j6NDZzRU',
-    services: [
-        ['browserstack', {
-            app: './apps/wikipedia.apk',
-            buildIdentifier: '#${DATE_TIME} #${BUILD_NUMBER}'
-        }]
+  ...baseConfig,
+  user: process.env.BROWSERSTACK_USERNAME,
+  key: process.env.BROWSERSTACK_ACCESS_KEY,
+  services: [
+    [
+      'browserstack',
+      {
+        app: './apps/wikipedia.apk',
+        buildIdentifier: '#${DATE_TIME} #${BUILD_NUMBER}',
+      },
     ],
-    capabilities: [{
-        "platformName": "android",
-        "appium:platformVersion": "12.0",
-        "appium:deviceName": "Samsung Galaxy S22 Ultra",
-        "bstack:options": {
-            "debug": "true",
-            "networkLogs": "true",
-            "projectName": "Onboarding",
-            "buildName": "Test build"
-        }
-    }]
-}
+  ],
+  capabilities: [
+    {
+      platformName: 'android',
+      'appium:platformVersion': '12.0',
+      'appium:deviceName': 'Samsung Galaxy S22 Ultra',
+      'bstack:options': {
+        debug: 'true',
+        networkLogs: 'true',
+        projectName: 'Onboarding',
+        buildName: 'Test build',
+      },
+    },
+  ],
+};
