fd9a50943c9604f30d71dcfbba001d047d39a052
Arkadiusz Buchholz <buchholzarkadiusz@gmail.com>
2024-11-28 11:50:15 +0100

    Remove creds from repo

--- src/tests/onboarding2.spec.ts
+++
@@ -1,9 +0,0 @@
-import Home from '../support/screens/Home/Home.assertion';
-// import Onboarding from '../support/screens/Onboarding/Onboarding.screen';
-
-describe('Onboarding2', () => {
-  it('Should complete onboarding2', async () => {
-    // test await Onboarding.complete();
-    await Home.assert();
-  });
-});
