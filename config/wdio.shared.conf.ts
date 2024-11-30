287b45225e59a12d4aff9bbdaf826de59427eda2
Arkadiusz Buchholz <buchholzarkadiusz@gmail.com>
2024-11-30 11:03:23 +0100

    Add workflow for BrowserStack

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -1,20 +1,21 @@
+import type { Options } from '@wdio/types';
 
-export const config: WebdriverIO.Config = {
-    runner: 'local',
-    specs: ['../src/tests/**/*.spec.ts'],
-    exclude: [],
-    maxInstances: 1,
-    capabilities: [],
-    logLevel: 'info',
-    bail: 0,
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    services: [],
-    framework: 'mocha',
-    reporters: ['spec'],
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
-    },
-}
+export const config: Options.Testrunner = {
+  runner: 'local',
+  specs: ['../src/tests/**/*.spec.ts'],
+  exclude: [],
+  maxInstances: 1,
+  capabilities: [],
+  logLevel: 'info',
+  bail: 0,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: [],
+  framework: 'mocha',
+  reporters: ['spec'],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000,
+  },
+};
